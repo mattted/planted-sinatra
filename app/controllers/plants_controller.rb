64df94227848a0d59840244861e89f4b355bf306
mattted <medward4@gmail.com>
2020-03-31 14:33:56 -0700

    Add helpers for permissions

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,46 +1,41 @@
 class PlantsController < ApplicationController
 
   get '/plants/new' do
-    if !logged_in?
-      flash[:message] = "Must be logged in to add plants"
-      redirect "/login"
-    else
-      erb :'/plants/new'
-    end
+    erb :'/plants/new' if logged_in?
   end
 
   get '/plants/:id/edit' do
-    @plant = Plant.find_by_id(params[:id])
-    if @plant.nil?
-      flash[:message] = "No plant with that id"
-      redirect '/home'
-    else
-      erb :'/plants/edit'
-    end
+    @plant = exists? if logged_in? && exists? && permission?
+    erb :'/plants/edit' 
+  end
+
+  get '/plants/:id/delete' do
+    @plant = exists? if logged_in? && exists? && permission?
+    erb :'/plants/delete'
   end
 
   get '/plants/:id' do
-    @plant = Plant.find_by_id(params[:id])
-    if @plant.nil?
-      flash[:message] = "No plant with that id"
-      redirect '/home'
+    @plant = exists?
+    erb :'/plants/plant'
+  end
+
+  delete '/plants' do
+    @plant = exists? if logged_in? && exists? && permission?
+
+    if params[:delete] == "no"
+      redirect "/plants/#{@plant.id}"
     else
-      erb :'/plants/plant'
+      @plant.delete
+      flash[:message] = "Plant deleted"
+      redirect "/home"
     end
   end
 
   patch '/plants' do
-    @plant = Plant.find_by_id(params[:plant][:id]) 
-
-    if @plant.nil?
-      flash[:message] = "No plant with that id"
-      redirect '/home'
-    else
-      @plant.update(params[:plant])
-      flash[:message] = "Plant successfully edited"
-      redirect "/plants/#{@plant.id}"
-    end
-
+    @plant = exists? if logged_in? && exists? && permission?
+    @plant.update(params[:plant])
+    flash[:message] = "Plant successfully edited"
+    redirect "/plants/#{@plant.id}"
   end
 
   post '/plants' do
@@ -50,6 +45,37 @@ class PlantsController < ApplicationController
 
     flash[:message] = "Plant created"
     redirect "/plants/#{@plant.id}"
+  end
+
+  helpers do
+    
+    def logged_in?
+      if !session[:uid]
+        flash[:message] = "Must be logged in to add plants"
+        redirect "/login"
+      else
+        !!session[:uid]
+      end
+    end
+
+    def exists?
+      if plant = Plant.find_by_id(params[:id]) 
+        plant
+      else
+        flash[:message] = "No plant with that id in the database"
+        redirect '/home'
+      end
+    end
+
+    def permission?
+      if user = Plant.find_by_id(params[:id]).users.find_by(id: session[:uid])
+        user
+      else
+        flash[:message] = "You do not have permission to edit this plant"
+        redirect '/home'
+      end
+    end
+
   end
 
 end
