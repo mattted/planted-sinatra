a8fbd0bcccee72cfdc9334040970f33277431a79
mattted <medward4@gmail.com>
2020-03-31 10:09:21 -0700

    Adjust attributes in plant table and forms

--- db/migrate/20200331050457_create_plants_table.rb
+++ db/migrate/20200331050457_create_plants_table.rb
@@ -2,13 +2,15 @@ class CreatePlantsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :plants do |t|
       t.string :name
+      t.string :cname
       t.string :sci_name
+      t.date :date
+      t.boolean :water_track
+      t.float :water
+      t.boolean :fert_track
+      t.float :fert
       t.string :light
-      t.string :water
-      t.string :fertilizer
-      t.string :temperature
       t.string :humidity
-      t.integer :user_id
     end
   end
 end
