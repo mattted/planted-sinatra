646c2b1c28e17a63386ec17ae69580f1b76f7f30
mattted <medward4@gmail.com>
2020-03-31 12:28:13 -0700

    Add plant edit functionality

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -9,11 +9,38 @@ class PlantsController < ApplicationController
     end
   end
 
+  get '/plants/:id/edit' do
+    @plant = Plant.find_by_id(params[:id])
+    if @plant.nil?
+      flash[:message] = "No plant with that id"
+      redirect '/home'
+    else
+      erb :'/plants/edit'
+    end
+  end
+
   get '/plants/:id' do
     @plant = Plant.find_by_id(params[:id])
-    redirect '/home' if @plant.nil?
+    if @plant.nil?
+      flash[:message] = "No plant with that id"
+      redirect '/home'
+    else
+      erb :'/plants/plant'
+    end
+  end
 
-    erb :'/plants/plant'
+  patch '/plants' do
+    @plant = Plant.find_by_id(params[:plant][:id]) 
+
+    if @plant.nil?
+      flash[:message] = "No plant with that id"
+      redirect '/home'
+    else
+      @plant.update(params[:plant])
+      flash[:message] = "Plant successfully edited"
+      redirect "/plants/#{@plant.id}"
+    end
+
   end
 
   post '/plants' do
