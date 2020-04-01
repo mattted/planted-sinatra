98c43f0b4955e57e1298628fba2ed684ff9f3331
mattted <medward4@gmail.com>
2020-03-31 19:17:28 -0700

    Use relative path to import csv for database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 require 'csv'
 require 'pry'
 
-csv_text = File.read('/home/matted/Downloads/Plants19-11-26.csv')
+
+csv_text = File.read(__dir__ + '/csv/Plants19-11-26.csv')
 csv = CSV.parse(csv_text, headers: true)
 user = User.first
 csv.each do |row|
